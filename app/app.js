26f693405a59c81330724b808530a30391957c7f
vardjaklap <vardjaklap@gmail.com>
2018-01-02 11:43:48 +0200

    firs commit

---
+++ app/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+
+    $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+
+$('.navOverlay').mouseenter(function(){
+    $(this).addClass('shine');
+});
+$('.navOverlay').mouseleave(function(){
+    $(this).removeClass('shine');
+});
